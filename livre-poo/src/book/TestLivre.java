85389e5d8103b6b2b198c13bfb705ac3672076ff
anisskiller <anissaallou36@gmail.com>
2018-11-19 01:15:54 +0100

    Update TestLivre.java

--- livre-poo/src/book/TestLivre.java
+++ livre-poo/src/book/TestLivre.java
@@ -7,23 +7,24 @@ public class TestLivre {
 	public static void main(String[] args) {
 		Livre[] livres = new Livre[3];
 		Scanner sc = null; 
-		System.out.println("Combien de livres à gérer ? ");
+		System.out.println("Combien de livres Ã  gÃ©rer ? ");
 		for (int i = 0; i < livres.length; i++) {
 			sc = new Scanner(System.in);
-			System.out.print("Donner le titre du livre n°  " + (i + 1) + " :");
+			System.out.print("Donner le titre du livre numÃ©ro " + (i + 1) + " :");
 			String titre = sc.nextLine();
-			System.out.print("Donner l'auteur du livre n°  " + (i + 1) + " :");
+			System.out.print("Donner l'auteur du livre numÃ©ro " + (i + 1) + " :");
 			String auteur = sc.nextLine();
-			System.out.print("Donner le prix du livre n°  " + (i + 1) + " :");
+			System.out.print("Donner le prix du livre numÃ©ro " + (i + 1) + " :");
 			int prix = sc.nextInt();
 			// instanciation de la classeLivre qui prend comme arguments titre, auteur, prix
 			livres[i] = new Livre(titre, auteur, prix);
 		}
 		sc.close();
-		for (Livre l : livres)
+		for (Livre l : livres) {
 			System.out.println(l);
+		}
  
-		System.out.println("Le nombre de livres est " + Livre.count);
+		System.out.println("Le nombre de livres est : " + Livre.count);
 	}
 
 }
